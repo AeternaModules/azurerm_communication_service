4d41f6f1bb45f79a0de2d702d6a1f54f27a131f9
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:36:23 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "communication_services" {
+  description = "All communication_service resources"
+  value       = azurerm_communication_service.communication_services
+}
+
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "communication_services" {
+  description = <<EOT
+Map of communication_services, attributes below
+Required:
+    - name
+    - resource_group_name
+Optional:
+    - data_location
+    - tags
+EOT
+
+  type = map(object({
+    name                = string
+    resource_group_name = string
+    data_location       = optional(string, "United States")
+    tags                = optional(map(string))
+  }))
+}
+
