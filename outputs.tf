35637fbe4ab684ddd29fdba88360a0b71f1eb741
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,46 +1,41 @@
-output "communication_services" {
-  description = "All communication_service resources"
-  value       = azurerm_communication_service.communication_services
-  sensitive   = true
-}
 output "communication_services_data_location" {
-  description = "List of data_location values across all communication_services"
-  value       = [for k, v in azurerm_communication_service.communication_services : v.data_location]
+  description = "Map of data_location values across all communication_services, keyed the same as var.communication_services"
+  value       = { for k, v in azurerm_communication_service.communication_services : k => v.data_location }
 }
 output "communication_services_hostname" {
-  description = "List of hostname values across all communication_services"
-  value       = [for k, v in azurerm_communication_service.communication_services : v.hostname]
+  description = "Map of hostname values across all communication_services, keyed the same as var.communication_services"
+  value       = { for k, v in azurerm_communication_service.communication_services : k => v.hostname }
 }
 output "communication_services_name" {
-  description = "List of name values across all communication_services"
-  value       = [for k, v in azurerm_communication_service.communication_services : v.name]
+  description = "Map of name values across all communication_services, keyed the same as var.communication_services"
+  value       = { for k, v in azurerm_communication_service.communication_services : k => v.name }
 }
 output "communication_services_primary_connection_string" {
-  description = "List of primary_connection_string values across all communication_services"
-  value       = [for k, v in azurerm_communication_service.communication_services : v.primary_connection_string]
+  description = "Map of primary_connection_string values across all communication_services, keyed the same as var.communication_services"
+  value       = { for k, v in azurerm_communication_service.communication_services : k => v.primary_connection_string }
   sensitive   = true
 }
 output "communication_services_primary_key" {
-  description = "List of primary_key values across all communication_services"
-  value       = [for k, v in azurerm_communication_service.communication_services : v.primary_key]
+  description = "Map of primary_key values across all communication_services, keyed the same as var.communication_services"
+  value       = { for k, v in azurerm_communication_service.communication_services : k => v.primary_key }
   sensitive   = true
 }
 output "communication_services_resource_group_name" {
-  description = "List of resource_group_name values across all communication_services"
-  value       = [for k, v in azurerm_communication_service.communication_services : v.resource_group_name]
+  description = "Map of resource_group_name values across all communication_services, keyed the same as var.communication_services"
+  value       = { for k, v in azurerm_communication_service.communication_services : k => v.resource_group_name }
 }
 output "communication_services_secondary_connection_string" {
-  description = "List of secondary_connection_string values across all communication_services"
-  value       = [for k, v in azurerm_communication_service.communication_services : v.secondary_connection_string]
+  description = "Map of secondary_connection_string values across all communication_services, keyed the same as var.communication_services"
+  value       = { for k, v in azurerm_communication_service.communication_services : k => v.secondary_connection_string }
   sensitive   = true
 }
 output "communication_services_secondary_key" {
-  description = "List of secondary_key values across all communication_services"
-  value       = [for k, v in azurerm_communication_service.communication_services : v.secondary_key]
+  description = "Map of secondary_key values across all communication_services, keyed the same as var.communication_services"
+  value       = { for k, v in azurerm_communication_service.communication_services : k => v.secondary_key }
   sensitive   = true
 }
 output "communication_services_tags" {
-  description = "List of tags values across all communication_services"
-  value       = [for k, v in azurerm_communication_service.communication_services : v.tags]
+  description = "Map of tags values across all communication_services, keyed the same as var.communication_services"
+  value       = { for k, v in azurerm_communication_service.communication_services : k => v.tags }
 }
 
